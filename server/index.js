019949fb6f608dce1560a0323b4a33d107ad8746
Johnny Chen <mcjohnnychen@gmail.com>
2017-11-15 14:15:28 -0800

    Add express server

---
+++ server/index.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var path = require('path');
+var bodyParser = require('body-parser');
+
+var app = express();
+
+// define port
+var port = 3000;
+app.listen(3000);
+
+// middleware
+app.use(express.static(path.join(__dirname, '../client/dist'))); // add static directory to serve files
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
