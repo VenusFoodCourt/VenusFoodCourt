235c7c7044aeb7ecbf231c741a91d07b65d7112b
Johnny Chen <mcjohnnychen@gmail.com>
2017-11-20 11:39:42 -0800

    Fix npm run react-dev error

--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,10 @@ var config = {
       {
         test : /\.jsx?/,
         include : APP_DIR,
-        loader : 'babel-loader'
+        loader : 'babel-loader',
+        query: {
+          presets: ['react', 'es2015']
+        }
       }
     ]
   }
